d7d711ce4f65f64ba5aa13c9ccb48fa4bf5641db
vivienbcr <vivien.boucher@outlook.com>
2021-03-31 16:09:09 +0000

    rip label

--- main.tf
+++ main.tf
@@ -39,8 +39,6 @@ resource "google_compute_instance" "hashicat" {
   name         = "${var.prefix}-hashicat"
   zone         = "${var.region}-b"
   machine_type = var.machine_type
-  department = "devops"
-  billable="true"
   
 
   boot_disk {
@@ -63,6 +61,8 @@ resource "google_compute_instance" "hashicat" {
 
   labels = {
     name = "hashicat"
+    department = "devops"
+    billable="true"
   }
 
 }
